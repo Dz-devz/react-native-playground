073fee7be3a37af48bfc9d70a28fff72a0551f71
Dz-devz <darwinbjordan@gmail.com>
2024-11-19 19:46:56 +0800

    explore: changing tabs and addind tabs and use nativewind

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,10 @@
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function TabTwoScreen() {
+export default function TabThreeScreen() {
   return (
   <SafeAreaView>
-  <Text className='text-2xl font-bold text-white'>Hello</Text>
+  <Text className='text-2xl text-white font-bold'>Profile: Darwin</Text>
  </SafeAreaView>   
 );
 }
